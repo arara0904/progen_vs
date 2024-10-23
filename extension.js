623177185e49ea15f96cfaaf36d3bbf10a475618
arara0904 <arajyn0904@gmail.com>
2024-10-24 08:49:01 +0900

    add source files

---
+++ extension.js
@@ -0,0 +1,53 @@
+const vscode = require('vscode');
+
+function run() {
+    vscode.window.showInformationMessage('Hello, world!');
+}
+
+const treeData =
+    [
+        {
+	    label: "run",
+	    collapsibleState: vscode.TreeItemCollapsibleState.None,
+	    command:{
+		command: "progen.run",
+		title: "run",
+		arguments: []
+	    }
+	},
+	{
+	    label: "export",
+	    collapsibleState: vscode.TreeItemCollapsibleState.None,
+	    command:{
+		command: "progen.export",
+		title: "export",
+		arguments: []
+	    }
+        }
+ 
+    ];
+
+class TreeDataProvider {
+    getTreeItem(element)  {
+        return element;
+    }
+
+    getChildren(element) {
+        if (!element) {
+            return treeData;
+        } else {
+            return element.children;
+        }
+    }
+}
+
+function activate(context) {
+    context.subscriptions.push(vscode.commands.registerCommand('progen.run', run);
+    vscode.window.registerTreeDataProvider('progen', new TreeDataProvider());
+}
+
+function deactivate() {
+    return undefined;
+}
+
+module.exports = { activate, deactivate };
